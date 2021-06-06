ba66e59663bed0e3f241b1c50d81b3ffc5b14222
Andrei <6abc6@mail.ru>
2021-06-06 21:37:50 +0300

    create APIHandler

---
+++ TestApp/Common files/Alert.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+struct Alert {
+
+    static func locationServiceIsDesable(_ sender: UIViewController) {
+        
+        let title = "Location Service are DISABLE"
+        let message = "Please enable Location Service in your Settings"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        sender.present(alert, animated: true, completion: nil)
+    }
+    
+    static func noInternetConnection(_ sender: UIViewController) {
+        let title = "No Internet Connection"
+        let message = "Please connect to Internet"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        sender.present(alert, animated: true, completion: nil)
+    }
+}
---
+++ TestApp/NetworkService/APIHandler.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+import CoreLocation
+
+struct APIHandler {
+    static weak var viewController: UIViewController?
+    static private var alertHasShow = false
+    
+    static func request(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping(Data?, Error?)-> Void) {
+        
+        let optURL = URLBuilder()
+            .set(scheme: Constants.scheme)
+            .set(host: Constants.host)
+            .set(path: Constants.path + "onecall")
+            .addQueryItem(name: "lat", value: "\(latitude)")
+            .addQueryItem(name: "lon", value: "\(longitude)")
+            .addQueryItem(name: "units", value: "\(Constants.metricFormat)")
+            .addQueryItem(name: "appid", value: "\(Constants.apiKey)")
+            .build()
+        
+        guard let url = optURL else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            switch error {
+            case nil:
+                completion(data, error)
+            default:
+                if alertHasShow == false {
+                    guard let viewController = viewController else {return}
+                    DispatchQueue.main.async {
+                        // TODO: write Alert
+                        
+                        alertHasShow = true
+                    }
+                }
+            }
+        }.resume()
+    }
+}
